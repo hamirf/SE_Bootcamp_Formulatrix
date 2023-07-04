95134680d678ae23331c3f1b08f619c04f9f68b9
hamirf <haidar300400@gmail.com>
2023-07-04 12:09:59 +0700

    delete

--- Lectures/Day 1 Week 1/Day 1/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello There, How's it going?");
--- Lectures/Day 1 Week 1/Initial C# Learning/Display.cs
+++
@@ -1,8 +0,0 @@
-namespace withClass;
-
-public class Display
-{
-    public static void theDisplay() {
-        Console.WriteLine("This is Display class");
-    }
-}
