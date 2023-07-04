5e1d89bad09c2dbca81bdc628e34660da275cc32
hamirf <haidar300400@gmail.com>
2023-07-04 21:57:52 +0700

    Add some line in Static Modifier Project

--- Lectures/Week 1/Day 2/Static Modifier/Calculator.cs
+++ Lectures/Week 1/Day 2/Static Modifier/Calculator.cs
@@ -2,28 +2,29 @@ namespace Static;
 
 public class Calculator
 {
-	public static int calcSize;
-	public int calcSizeNotStatic;
-	
-	public Calculator(int calcSizeNotStatic) {
-		this.calcSizeNotStatic = calcSizeNotStatic;
-		//this.calcSize  = calcSize; //error
-		calcSize = calcSizeNotStatic;
-	}
-	
-	public static int Add(int a, int b) 
-	{
-		return a * b;
-	}
-	public int ReturnSize() 
-	{
-		return calcSize;
-		//return calcSizeNotStatic; //not error
-	}
-	
-	static public int ReturnSizeCalc() 
-	{
-		//return calcSizeNotStatic; //error
-		return calcSize;
-	}
+    public static int calcSize;
+    public int calcSizeNotStatic;
+
+    public Calculator(int calcSizeNotStatic)
+    {
+        this.calcSizeNotStatic = calcSizeNotStatic;
+        //this.calcSize  = calcSize; //error
+        calcSize = calcSizeNotStatic;
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a * b;
+    }
+    public int ReturnSize()
+    {
+        return calcSize;
+        //return calcSizeNotStatic; //not error
+    }
+
+    static public int ReturnSizeCalc()
+    {
+        //return calcSizeNotStatic; //error, method static hanya bisa mengakses variable static
+        return calcSize;
+    }
 }
--- Lectures/Week 1/Day 2/Static Modifier/Program.cs
+++ Lectures/Week 1/Day 2/Static Modifier/Program.cs
@@ -1,24 +1,37 @@
 ﻿using Static;
 
-class Program 
+class Program
 {
-	static void Main() 
-	{
+    static void Main()
+    {
+        // int a = 1;
+        // int b = 2;
         //calcSizeNotStatic = 2, calcSize = 2
-		Calculator calculator = new Calculator(2);
+        Calculator calculator = new Calculator(2);
 
         //calcSizeNotStatic = 1, calcSize = 1
-		Calculator calculator1= new Calculator(1);
+        Calculator calculator1 = new Calculator(1);
 
         //Non-static class without instance, static method; calcSize = 1
-		Console.WriteLine(Calculator.ReturnSizeCalc());
+        Console.WriteLine(Calculator.ReturnSizeCalc());
         //Non-static class with instance, non-static method; calcSize = 1
-		Console.WriteLine(calculator.ReturnSize());
+        Console.WriteLine(calculator.ReturnSize());
         //Non-static class with instance, non-static method; calcSize = 1
-		Console.WriteLine(calculator1.ReturnSize());
-        //Non-static class with instance, static method; output error. Karena instance/objek tidak bisa mengubah static method
+        Console.WriteLine(calculator1.ReturnSize());
+        //Non-static class with instance, static method; output error. Karena instance/objek tidak bisa mengubah/mengakses static method
         // Console.WriteLine(calculator.ReturnSizeCalc());
-        //Instance want to change the value of static variable (calcSize), error Karena instance/objek tidak bisa mengubah static variable
+        //Instance want to change the value of static variable (calcSize), error Karena instance/objek tidak bisa mengubah/mengakses static variable
         // calculator1.calcSize = 2;
-	}
+        // Console.WriteLine(Calculator.Add(a,b));
+        calculator.calcSizeNotStatic = 2;
+        Console.WriteLine(calculator.calcSizeNotStatic);
+
+        Console.WriteLine();
+        //Non-static class without instance, static method; calcSize = 1
+        Console.WriteLine(Calculator.ReturnSizeCalc());
+        //Non-static class with instance, non-static method; calcSize = 1
+        Console.WriteLine(calculator.ReturnSize());
+        //Non-static class with instance, non-static method; calcSize = 1
+        Console.WriteLine(calculator1.ReturnSize());
+    }
 }
