a88caf05c2226166c4231bd43d94bb87b69cba5e
hamirf <haidar300400@gmail.com>
2023-07-06 17:27:57 +0700

    Add UpcastDowncast, Boxing, and Dynamic

---
+++ Lectures/Week 1/Day 4/004_UpcastDowncast/Program.cs
@@ -0,0 +1,60 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        int a = 2;
+        double b = a;
+
+        double c = 7.5;
+        int d = (int)c;
+
+        Parent parent = new Parent();
+        Child child = new Child();
+
+        parent.MethodParentB();
+        parent.MethodParentA();
+
+        child.MethodParentB();
+        child.MethodParentA();
+        child.MethodChildA();
+        child.MethodChildB();
+
+        Parent parent2 = child;
+        parent2.MethodParentB();
+        parent2.MethodParentA();
+        // Error, karena parent tidak memiliki dan tidak mewarisi method dari child 
+        //parent2.MethodChildA();
+        //parent2.MethodChildB();
+
+        // Error, tidak bisa cast dari parent ke child
+        Child child1 = (Child)parent;
+        child1.MethodParentB();
+        child1.MethodParentA();
+        child1.MethodChildB();
+        child1.MethodChildA();
+    }
+}
+
+public class Parent
+{
+    public void MethodParentA()
+    {
+        Console.WriteLine("Ini Method Parent A");
+    }
+    public void MethodParentB()
+    {
+        Console.WriteLine("Ini Method Parent B");
+    }
+}
+public class Child : Parent
+{
+    public void MethodChildA()
+    {
+        Console.WriteLine("Ini Method Child A");
+    }
+    public void MethodChildB()
+    {
+        Console.WriteLine("Ini Method Child B");
+    }
+}
---
+++ Lectures/Week 1/Day 4/005_Boxing/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Object
+{
+    static class Program
+    {
+        static void Main()
+        {
+            int x = 2;
+            object obj1 = x; // boxing
+            int y = (int)obj1; // unboxing
+            Console.WriteLine(y);
+
+            int a = 10;
+            object obj2 = a;
+            //string str1 = (string)obj2;
+            string str = ((int)obj2).ToString(); // unboxing convert
+            Console.WriteLine(str);
+
+            int c = 11;
+            object obj3 = c;
+            //long d = (long)(int)obj3; 
+            long d = (int)obj3; // unboxing upcast
+            Console.WriteLine(d);
+
+            float e = 12.2f;
+            object obj6 = e;
+            //int i = (int)obj6; 
+            int i = (int)(float)obj6; // unboxing downcast
+            Console.WriteLine(i);
+
+            object obj4 = "Hello, Asuka";
+            // Unboxing manual
+            if (obj4 is string)
+            {
+                string str4 = (string)obj4;
+                Console.WriteLine(str4);
+            }
+
+            object obj5 = "Hello, World! 2";
+            // Unboxing otomatis langsung tertampung
+            if (obj5 is string str5) //pattern matching
+            {
+                Console.WriteLine(str5);
+            }
+
+            object obj7 = 22;
+            // Bentuk penulisan lain dari pengkondisian If
+            int j = obj7 as int? ?? 0;
+            Console.WriteLine(j);
+        }
+    }
+}
