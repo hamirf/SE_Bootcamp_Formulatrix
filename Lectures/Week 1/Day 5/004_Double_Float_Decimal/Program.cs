c2ff6af238b2b569345d8d908c68ccbd65a572ba
hamirf <haidar300400@gmail.com>
2023-07-07 10:36:40 +0700

    Rev StringBuilder

---
+++ Lectures/Week 1/Day 5/004_Double_Float_Decimal/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Akurasi kurang bagus
+        double a = 2.1;
+        double b = 2.2;
+
+        float c = 3.3f;
+        float d = 3.4f;
+
+        // Best akurasi atau ketepatan
+        decimal e = 4.5M;
+        decimal f = 4.6M;
+
+        if (a + b == 4.3)
+        {
+            Console.WriteLine(true);
+        }
+        else
+        {
+            Console.WriteLine(false);
+        }
+
+        if (c + d == 6.7f)
+        {
+            Console.WriteLine(true);
+        }
+        else
+        {
+            Console.WriteLine(false);
+        }
+
+        if (e + f == 9.1M)
+        {
+            Console.WriteLine(true);
+        }
+        else
+        {
+            Console.WriteLine(false);
+        }
+    }
+}
---
+++ Lectures/Week 1/Day 5/005_StringBuilder/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder sb = new();
+        sb.Append("Hello ");
+        sb.AppendLine("There!");
+        sb.Append("C# ");
+        sb.Append("Here");
+        Console.WriteLine(sb);
+        Console.WriteLine("-------------");
+
+        sb.AppendFormat(" {0:C} ", 22);
+        Console.WriteLine(sb);
+        Console.WriteLine("-------------");
+
+        sb.Insert(0, "Oy, ");
+        Console.WriteLine(sb);
+        Console.WriteLine("-------------");
+
+        sb.Remove(15, 1);
+        Console.WriteLine(sb);
+        Console.WriteLine("-------------");
+
+        sb.Replace("There", "Asuka");
+        Console.WriteLine(sb);
+        Console.WriteLine("-------------");
+
+        int iterations = 100000;
+        string resultString = "";
+        StringBuilder resultStringBuilder = new StringBuilder();
+
+        Stopwatch stopwatch1 = Stopwatch.StartNew();
+        for (int i = 0; i < iterations; i++)
+        {
+            resultString += "a";
+        }
+        stopwatch1.Stop();
+
+        Console.WriteLine($"Time String : {stopwatch1.ElapsedMilliseconds} ms");
+
+        Stopwatch stopwatch2 = Stopwatch.StartNew();
+        for (int i = 0; i < iterations; i++)
+        {
+            resultStringBuilder.Append("a");
+        }
+        stopwatch2.Stop();
+
+        Console.WriteLine($"Time SB: {stopwatch2.ElapsedMilliseconds} ms");
+    }
+}
