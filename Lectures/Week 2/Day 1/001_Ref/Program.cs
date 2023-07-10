e368d49dada4bcface994f8f5023f3f08a26d62f
hamirf <haidar300400@gmail.com>
2023-07-10 12:01:26 +0700

    y

---
+++ Lectures/Week 2/Day 1/001_Ref/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Ref harus memiliki nilai awal
+        int num = 2;
+        int res = Method(num);
+        Console.WriteLine(res);
+
+        RefMethod(num);
+        Console.WriteLine(num);
+    }
+
+    static int Method(int x)
+    {
+        return x + 7;
+    }
+    // Variabel Ref bisa diubah ataupun tidak
+    // Variabel Ref bisa juga digunakan untuk perhitungan
+    static void OutMethod(ref int x)
+    {
+        return x + 7;
+    }
+}
+
+// class Ref
+// {
+
+// }
---
+++ Lectures/Week 2/Day 1/002_Out/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Out tidak harus memiliki nilai awal
+        // int num;
+        // int res = Method(num);
+        // Console.WriteLine(res);
+
+        OutMethod(out int num);
+        Console.WriteLine(num);
+    }
+
+    static int Method(int x)
+    {
+        return x + 7;
+    }
+    // Variabel Out harus memiliki output
+    // Variabel Ref bisa juga digunakan untuk perhitungan
+    static void OutMethod(out int x)
+    {
+        x = 7;
+    }
+}
+
+// class Ref
+// {
+
+// }
