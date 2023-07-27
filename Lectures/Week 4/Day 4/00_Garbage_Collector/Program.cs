780fbfa670097efdb972642705fee870bc7073d0
hamirf <haidar300400@gmail.com>
2023-07-27 12:05:03 +0700

    Add W4D4

---
+++ Lectures/Week 4/Day 4/00_Garbage_Collector/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main()
+    {
+        CreateProcessor();
+        // Processor processor = new(2, 4);
+        // Console.WriteLine("Total Memory:" + GC.GetTotalMemory(false));
+        GC.Collect();
+        // GC.WaitForPendingFinalizers();
+        // Console.WriteLine("Total Memory:" + GC.GetTotalMemory(false));
+    }
+
+    static void CreateProcessor()
+    {
+        for (int i = 0; i < 100000; i++)
+        {
+            Processor processor = new();
+        }
+    }
+}
+
+class Processor
+{
+    int core;
+    int thread;
+
+    public Processor()
+    {
+    }
+
+    public Processor(int core, int thread)
+    {
+        this.core = core;
+        this.thread = thread;
+        System.Console.WriteLine($"Processor with {core} core(s) and {thread} thread(s) has been added");
+    }
+    ~Processor()
+    {
+        System.Console.WriteLine($"\nProcessor with {core} core(s) and {thread} thread(s) has been demolished");
+    }
+}
