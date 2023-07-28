9293cdd6d48348aeda329a3487ad941a2f168b01
hamirf <haidar300400@gmail.com>
2023-07-28 10:12:18 +0700

    Add Manual Dispose

---
+++ Lectures/Week 4/Day 4/01_Manual_Dispose/House.cs
@@ -0,0 +1,58 @@
+class House : IDisposable
+{
+    private string _address;
+    private bool _disposedValue;
+
+    public House()
+    {
+    }
+
+    public House(string address)
+    {
+        _address = address;
+    }
+
+    public string GetAddress()
+    {
+        return _address;
+    }
+
+    public void SetAddress(string address)
+    {
+        _address = address;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+                if (_address != null)
+                {
+                    _address = null;
+                    System.Console.WriteLine("Class disposed");
+                }
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _disposedValue = true;
+        }
+    }
+
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // ~House()
+    // {
+    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ Lectures/Week 4/Day 4/01_Manual_Dispose/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+using System;
+class Program
+{
+    static void Main()
+    {
+        House rumah1 = new("Venice");
+        System.Console.WriteLine(rumah1.GetAddress());
+        rumah1.Dispose();
+        System.Console.WriteLine(rumah1.GetAddress());
+        System.Console.WriteLine("---");
+
+        using (House rumah2 = new("Dallas"))
+        {
+            System.Console.WriteLine(rumah2.GetAddress());
+        }
+    }
+}
